e384e194cf162e65a2fd324bb9cbbfc3c08e34e2
Miguel <migueleduagui@gmail.com>
2023-01-22 19:39:44 -0300

    fix api

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,4 +1,4 @@
-// const urlVideo= `https://api.themoviedb.org/3/movie/${id}/videos?api_key=`;
+// const urlVideo= `https://api.themoviedb.org/3/movie/${id}/videos?api_key=06b042254658e847272c1a8bf7fe0fb5`;
 //https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
 
 //para obtener el video de youtube, usar https://www.youtube.com/watch?v= seguido del key del video.
@@ -6,7 +6,7 @@
 const API_KEY = process.env.API_KEY;
 
 const rutaApi = {
-    apiKey: API_KEY,
+    apiKey: 'api_key=06b042254658e847272c1a8bf7fe0fb5',
     mainUrl: 'https://api.themoviedb.org/3/movie/',
     searchUrl: 'https://api.themoviedb.org/3/search/movie/',
     page:'?page=',
